25210a2915e8d035b6bdcd868b7c2796fe04cc56
Sameen525 <bsef14m525@pucit.edu.pk>
2016-05-26 20:44:17 +0500

    Add files via upload

---
+++ client (3).py
@@ -0,0 +1,25 @@
+#!usr/bin/python
+from socket import *
+from threading import Thread
+import sys
+s = socket(AF_INET, SOCK_STREAM)
+host = 'localhost'
+port = 7777
+adr =(host, port)
+s.connect((host, port))
+
+
+def recv():
+   while True:
+       data= s.recv(1024)
+       if not data:
+           sys.exit(0)
+       print data
+
+Thread( target=recv).start()
+
+while True:
+    data = raw_input('')
+    s.send(data)
+
+s.close()
